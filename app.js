6904ee64a659f60217d3866310d939a056a4303f
bosa_john <bosacoin@gmail.com>
2018-11-03 17:14:02 -0400

    mongo config

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://bagz:secretpassword123@127.0.0.1:27017/merndb', { promiseLibrary: require('bluebird') })
+mongoose.connect('mongodb://bagz:secretpassword123@localhost:27017/merndb', { promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
